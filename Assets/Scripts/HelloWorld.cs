e77440e6d3863458adec72b04c142c1807b69605
Jason Sebastian Flynn <J.S.Flynn@outlook.com>
2019-01-28 20:49:24 +0000

    simple Hello World with unity

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	private void Start () {
+		print("Hello World! Im Working in Unity");
+	}
+}
